21410f85fb1be25790e5d5dc260dffdfc2ed1d92
rvelasquezq <rodrigovelas2@gmail.com>
2023-10-14 21:36:12 -0500

    Se añadio la clase ChatController del Chat web

---
+++ src/main/java/com/cursojava/curso/controllers/ChatController.java
@@ -0,0 +1,25 @@
+package com.cursojava.curso.controllers;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessague) {
+        return chatMessague;
+
+    }//Fin ChatMessage
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        //Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+}//Fin ChatController
