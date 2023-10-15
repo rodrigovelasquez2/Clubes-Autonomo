c6ceb57416cae51476566fcd66b8eabad7b897de
rvelasquezq <rodrigovelas2@gmail.com>
2023-10-15 11:28:10 -0500

    Se documento el codigo

---
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -0,0 +1,92 @@
+package com.cursojava.curso.controllers;
+
+import com.cursojava.curso.DAO.UsuarioDAO;
+import com.cursojava.curso.models.Usuario;
+import com.cursojava.curso.utils.JWTUtil;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Clase que maneja las solicitudes HTTP relacionadas con los usuarios.
+ * @autor Rodrigo Andres Velasquez Quiroz
+ * @date 27/09/2023
+ */
+@RestController
+public class UsuarioController {
+
+    @Autowired
+    @Qualifier("usuarioService")
+    private UsuarioDAO usuarioDAO;
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    /**
+     * Obtiene un usuario por su ID.
+     * @param id El ID del usuario a obtener.
+     * @return El usuario correspondiente al ID especificado.
+     */
+    @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.GET)
+    public Usuario getUsuario(@PathVariable long id) {
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucas@gmail.com");
+        usuario.setTelefono("09992929");
+        return usuario;
+    }
+
+    /**
+     * Obtiene la lista de usuarios.
+     * @param token El token de autorización.
+     * @return La lista de usuarios si el token es válido; de lo contrario, null.
+     */
+    @RequestMapping(value = "api/usuarios", method = RequestMethod.GET)
+    public List<Usuario> getUsuarios(@RequestHeader(value = "Authorization") String token) {
+        if (!validarToken(token)) {
+            return null; // Si el token no es válido, se retorna null
+        }
+        return usuarioDAO.getUsuarios();
+    }
+
+    /**
+     * Valida un token de autorización.
+     * @param token El token a validar.
+     * @return true si el token es válido; de lo contrario, false.
+     */
+    private boolean validarToken(String token) {
+        String usuarioid = jwtUtil.getKey(token);
+        return usuarioid != null; // Retorna true si el token es válido; de lo contrario, false
+    }
+
+    /**
+     * Registra un nuevo usuario en la base de datos.
+     * @param usuario El usuario a registrar.
+     */
+    @RequestMapping(value = "api/usuarios", method = RequestMethod.POST)
+    public void registrarUsuario(@RequestBody Usuario usuario) {
+        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+        String hash = argon2.hash(1, 1024, 1, usuario.getPassword());
+        usuario.setPassword(hash);
+        usuarioDAO.registrar(usuario);
+    }
+
+    /**
+     * Elimina un usuario por su ID.
+     * @param token El token de autorización.
+     * @param id El ID del usuario a eliminar.
+     */
+    @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.DELETE)
+    public void eliminar(@RequestHeader(value = "Authorization") String token, @PathVariable Long id) {
+        if (!validarToken(token)) {
+            return; // Si el token no es válido, se sale temprano sin ejecutar la eliminación
+        }
+        usuarioDAO.eliminar(id);
+    }
+}
