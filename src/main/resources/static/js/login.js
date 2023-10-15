aa463b460c8951aa66bde126e94f4dbfcff48408
rvelasquezq <rodrigovelas2@gmail.com>
2023-10-15 11:20:00 -0500

    Se documento con ChatGPT el login

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,31 +1,38 @@
-// Call the dataTables jQuery plugin
+// Esta función se llama cuando el DOM se ha cargado completamente y está listo.
 $(document).ready(function () {
-// TODO CODE ALL HERE
+    // TODO: Coloca aquí el código que debe ejecutarse cuando el DOM esté listo.
 });
 
+// Esta función asincrónica maneja el inicio de sesión de usuarios.
 async function iniciarSesion() {
+    // Crea un objeto para almacenar los datos del usuario.
     let datos = {};
     datos.email = document.getElementById('txtEmail').value;
     datos.password = document.getElementById('txtPassword').value;
 
-    //Llama al servidor: > el fetch se coloca tal cuual en el AuthController de login
+    // Envía una solicitud POST al servidor (similar al método 'login' del AuthController).
     const request = await fetch('api/login', {
         method: 'POST',
         headers: {
-            //Indica que usara Json
+            // Indica que estamos enviando y esperando datos en formato JSON.
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(datos)// Llama a la función y agarra cualquier objeto a string de JSON
+        body: JSON.stringify(datos) // Convierte el objeto de datos a una cadena JSON.
     });
-    const respuesta = await request.text(); // trae un listado de usuarios
 
+    // Espera la respuesta del servidor.
+    const respuesta = await request.text(); // Obtiene una lista de usuarios.
+
+    // Comprueba si el inicio de sesión fue exitoso.
     if (respuesta != 'FAIL') {
+        // Si es exitoso, almacena el token JWT y el correo del usuario en el almacenamiento local.
         localStorage.token = respuesta;
         localStorage.email = datos.email;
-        window.location.href = 'usuarios.html'
+        // Redirige al usuario a la página 'usuarios.html'.
+        window.location.href = 'usuarios.html';
     } else {
-        alert("Usuario o password incorrecto, digite correctamente!!!");
-    }//Fin else
-
-}//Fin cargarUsuarios
+        // Muestra un mensaje de error si el inicio de sesión falla.
+        alert("Usuario o contraseña incorrectos. ¡Por favor, ingrese la información correcta!");
+    }// Fin del bloque 'else'.
+}// Fin de la función 'iniciarSesion'
