f8848dbd529300cd87325941a44a1be46d676bde
rvelasquezq <rodrigovelas2@gmail.com>
2023-11-29 13:46:29 -0500

    Se corrigio el registro en blanco

--- src/main/resources/static/js/registrar.js
+++ src/main/resources/static/js/registrar.js
@@ -2,31 +2,42 @@
 $(document).ready(function () {
 // TODO CODE ALL HERE
 });
-
 async function registrarUsuario() {
+    //Guarda lo capturado al array Datos
     let datos = {};
     datos.nombre = document.getElementById('txtNombre').value;
     datos.apellido = document.getElementById('txtApellido').value;
     datos.email = document.getElementById('txtEmail').value;
     datos.password = document.getElementById('txtPassword').value;
 
-    let repetirPassword = document.getElementById('txtRepetirPassword').value;
+    // Validación de campos en blanco
+    if ( datos.nombre === '' || datos.apellido === '' || datos.email === '' || datos.password === ''){
+        alert('Por favor, completa todos los campos.');
+    }else{
+        let repetirPassword = document.getElementById('txtRepetirPassword').value;
 
-    if (repetirPassword != datos.password) {
-        alert('La contraseña que escribiste es diferente.');
-        return; //Corta la función
-    }
-    // el fetch se coloca tal cual en el AuthController de UsuarioController
-    const request = await fetch('api/usuarios', {//await: Esperar el resultado
-        method: 'POST',
-        headers: {
-            //Indica que usara Json
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(datos)// Llama a la funcion y agarra cualquier objeto a string de JSON
-    });
-    alert("La cuenta fue creada con exito!");
-    window.location.href = 'index.html'
+        if (repetirPassword != datos.password) {
+            alert('La contraseña que escribiste es diferente.');
+            return; //Corta la función
+        }
+        // el fetch se coloca tal cual en el AuthController de UsuarioController
+        const request = await fetch('api/usuarios', {//await: Esperar el resultado
+            method: 'POST',
+            headers: {
+                //Indica que usara Json
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(datos)// Llama a la funcion y agarra cualquier objeto a string de JSON
+        });
+        const respuesta = await request.text(); // Suponiendo que 'request' es tu solicitud y obtienes una respuesta en texto.
+        alert(respuesta);
+
+        alert("La cuenta fue creada con exito!");
+        window.location.href = 'index.html' // Posible cambiarlo
+
+    }//Fin if
+
+
 
 }//Fin cargarUsuarios
